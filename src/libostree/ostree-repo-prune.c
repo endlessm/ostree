ad37034ff1bb7aa0e917cf76a041f01042cd6c32
Dan Nicholson <nicholson@endlessm.com>
2015-03-16 11:13:21 -0700

    prune: Do full commit traversal
    To prune the signature and sizes files, a full commit traversal needs to
    be performed.
    
    [endlessm/eos-shell#2034]

--- src/libostree/ostree-repo-prune.c
+++ src/libostree/ostree-repo-prune.c
@@ -145,8 +145,9 @@ ostree_repo_prune (OstreeRepo        *self,
         {
           const char *checksum = value;
           
-          if (!ostree_repo_traverse_commit (self, checksum, depth, data.reachable,
-                                            cancellable, error))
+          if (!ostree_repo_traverse_commit_full (self, checksum, depth,
+                                                 data.reachable,
+                                                 cancellable, error))
             goto out;
         }
     }
@@ -169,8 +170,9 @@ ostree_repo_prune (OstreeRepo        *self,
           if (objtype != OSTREE_OBJECT_TYPE_COMMIT)
             continue;
           
-          if (!ostree_repo_traverse_commit (self, checksum, depth, data.reachable,
-                                            cancellable, error))
+          if (!ostree_repo_traverse_commit_full (self, checksum, depth,
+                                                 data.reachable,
+                                                 cancellable, error))
             goto out;
         }
     }
