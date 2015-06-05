1ec585976fb541745ed67d3ebd9c53de783f02eb
John Hiesey <john@hiesey.com>
2015-06-05 12:18:22 -0700

    Implement the size-summary command
    size-summary prints information about the download size, on disk size,
    and number of files for a given commit
    
    [endlessm/eos-shell#1965]

--- src/ostree/ot-builtins.h
+++ src/ostree/ot-builtins.h
@@ -48,6 +48,7 @@ BUILTINPROTO(fsck);
 BUILTINPROTO(show);
 BUILTINPROTO(static_delta);
 BUILTINPROTO(summary);
+BUILTINPROTO(size_summary);
 BUILTINPROTO(rev_parse);
 BUILTINPROTO(remote);
 BUILTINPROTO(write_refs);
