206e28996563158083bf97e16005702aa3713413
John Hiesey <john@hiesey.com>
2015-06-08 11:57:33 -0700

    Implement the size-summary command
    size-summary prints information about the download size, on disk size,
    and number of files for a given commit
    
    [endlessm/eos-shell#1965]
    
    Rebase 2016.9: Port from gs_unref_variant to g_autoptr and gs_free to
      g_autofree, drop unnecessary option handling, always free option
      context and use C-style comments
    Rebase 2016.15: Only return FALSE if get_commit_sizes() returns an
      error. It's not an error if no files were found in the commit. This
      fixes an assertion where ostree_run() expects that the command will
      either return TRUE or fill in the GError.
    Rebase 2017.6 (T17204): No changes.
    Rebase 2017.7 (T16919): Drop an unused variable

--- src/ostree/main.c
+++ src/ostree/main.c
@@ -58,6 +58,7 @@ static OstreeCommand commands[] = {
   { "show", ostree_builtin_show },
   { "static-delta", ostree_builtin_static_delta },
   { "summary", ostree_builtin_summary },
+  { "size-summary", ostree_builtin_size_summary },
 #if defined(HAVE_LIBSOUP) && defined(BUILDOPT_ENABLE_TRIVIAL_HTTPD_CMDLINE)
   { "trivial-httpd", ostree_builtin_trivial_httpd },
 #endif
