e512153cd3d56becb1ee1445b67bbae84256752c
Dan Nicholson <nicholson@endlessm.com>
2015-07-25 09:51:14 -0700

    deploy: Delete .updated file from /etc and /var on new deployments
    Systemd units using ConditionNeedsUpdate run if the mtime of .updated in
    the specified directory is newer than /usr. Since /usr has an mtime of
    0, there's no way to have an older .updated file. Systemd units
    typically specify ConditionNeedsUpdate=/etc or ConditionNeedsUpdate=/var
    to support stateless systems like ostree.
    
    Remove the file from the new deployment's /etc and the OS's /var
    regardless of where they came from to ensure that these systemd units
    run when booting new deployments. This will provide a method to run
    services only on upgrade.
    
    [endlessm/eos-shell#5426]

--- src/libostree/ostree-sysroot-deploy.c
+++ src/libostree/ostree-sysroot-deploy.c
@@ -2035,6 +2035,7 @@ ostree_sysroot_deploy_tree (OstreeSysroot     *self,
   g_autofree char *tree_kernel_path = NULL;
   g_autofree char *tree_initramfs_path = NULL;
   glnx_fd_close int deployment_dfd = -1;
+  glnx_fd_close int deployment_var_dfd = -1;
   glnx_unref_object OstreeSePolicy *sepolicy = NULL;
   g_autofree char *new_bootcsum = NULL;
   glnx_unref_object OstreeBootconfigParser *bootconfig = NULL;
@@ -2108,6 +2109,18 @@ ostree_sysroot_deploy_tree (OstreeSysroot     *self,
       g_prefix_error (error, "During /etc merge: ");
       goto out;
     }
+
+  /* Ensure that the new deployment does not have /etc/.updated or
+   * /var/.updated so that systemd ConditionNeedsUpdate=/etc|/var
+   * services run after rebooting.
+   */
+  if (!ot_ensure_unlinked_at (deployment_dfd, "etc/.updated", error))
+    goto out;
+  if (!gs_file_open_dir_fd (deployment_var, &deployment_var_dfd,
+                            cancellable, error))
+    goto out;
+  if (!ot_ensure_unlinked_at (deployment_var_dfd, ".updated", error))
+    goto out;
 
   g_clear_object (&self->sepolicy);
   self->sepolicy = g_object_ref (sepolicy);
