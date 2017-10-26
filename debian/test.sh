3186ca5f2e244760b4e721c63727d96a88e6ae04
Simon McVittie <smcv@debian.org>
2017-10-27 00:19:44 +0100

    d/test.sh: Don't repeat build-time tests if they fail once. They seem to be somewhat reliable now.

--- debian/test.sh
+++ debian/test.sh
@@ -13,19 +13,8 @@ unset http_proxy
 unset https_proxy
 unset no_proxy
 
-try_tests=5
-
 failed=0
 make check || failed=1
-
-if [ "$failed" -gt 0 ]; then
-    [ "$failed" -eq 0 ] || echo "Test failed! Checking how reproducible it is..."
-    for i in $(seq 1 "$(( $try_tests - 1 ))"); do
-        if ! make check; then
-            failed=$(( $failed + 1 ))
-        fi
-    done
-fi
 
 pkill --full "gpg-agent --homedir /var/tmp/tap-test\\.[^/]+/.*" || :
 pkill --full '\.libs/ostree-trivial-httpd' || :
@@ -37,18 +26,6 @@ if pgrep lt-ostree || pgrep --full '\.libs/ostree-trivial-httpd' || pgrep --full
     pgrep lt-ostree | xargs --no-run-if-empty ps ww
 fi
 
-# There are several race conditions that cause intermittent failures.
-# They are not actually a regression - we've just been luckier in the
-# past - so let newer versions build reliably.
-if [ "$failed" -gt 1 ]; then
-    echo "Failed $failed out of $try_tests test runs; that seems bad"
-    exit 1
-elif [ "$failed" -gt 0 ]; then
-    echo "Failed $failed out of $try_tests test runs; continuing anyway"
-else
-    echo "All tests passed"
-fi
-
-exit 0
+exit $failed
 
 # vim:set et sw=4 sts=4:
