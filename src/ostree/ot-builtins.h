df4f3c3255404806f23b40fef33e225ed7a53584
Dan Nicholson <nicholson@endlessm.com>
2018-04-16 11:46:08 -0500

    Implement the size-summary command
    size-summary prints information about the download size, on disk size,
    and number of files for a given commit
    
    [endlessm/eos-shell#1965]
    
    Rebase 2016.9: Port from gs_unref_variant to g_autoptr and gs_free to
      g_autofree, drop unnecessary option handling, always free option
      context and use C-style comments
    Rebase 2016.15: Only return FALSE if get_commit_sizes() returns an
      error. It's not an error if no files were found in the commit. This
      fixes an assertion where ostree_run() expects that the command will
      either return TRUE or fill in the GError.
    Rebase 2017.6 (T17204): No changes.
    Rebase 2017.7 (T16919): Drop an unused variable
    Rebase 2017.10 (T16919): Fix leak and prefer early return
    Rebase 2018.4 (T22022): Change size-summary option handling to use
      OstreeCommandInvocation and set description in OstreeCommand array.

--- src/ostree/ot-builtins.h
+++ src/ostree/ot-builtins.h
@@ -56,6 +56,7 @@ BUILTINPROTO(fsck);
 BUILTINPROTO(show);
 BUILTINPROTO(static_delta);
 BUILTINPROTO(summary);
+BUILTINPROTO(size_summary);
 BUILTINPROTO(rev_parse);
 BUILTINPROTO(remote);
 BUILTINPROTO(write_refs);
