37254102378c20cd406ada001890b38b353f0cac
Dan Nicholson <nicholson@endlessm.com>
2020-01-31 17:05:25 -0700

    Revert "Implement the size-summary command"
    This reverts commit 589df9809114c62bef7b6858cf53066e4add6893. It uses
    our downstream `ostree_repo_get_commit_sizes` API, which is being
    replaced with an upstream version. I doubt anyone uses this builtin, and
    it's coming back as the `--print-sizes` option to `ostree show` in
    2019.7.
    
    https://phabricator.endlessm.com/T18171

--- src/ostree/main.c
+++ src/ostree/main.c
@@ -118,9 +118,6 @@ static OstreeCommand commands[] = {
   { "summary", OSTREE_BUILTIN_FLAG_NONE,
     ostree_builtin_summary,
     "Manage summary metadata" },
-  { "size-summary", OSTREE_BUILTIN_FLAG_NONE,
-    ostree_builtin_size_summary,
-    "Display the summary for branch" },
 #if defined(HAVE_LIBSOUP) && defined(BUILDOPT_ENABLE_TRIVIAL_HTTPD_CMDLINE)
   { "trivial-httpd", OSTREE_BUILTIN_FLAG_NONE,
     ostree_builtin_trivial_httpd,
